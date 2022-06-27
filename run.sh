57eb611a1a19f1ec2060dd117e011bc623ae8077
Lukas Ehmann <ehmannlu@cp2019-13.tnt.uni-hannover.de>
2022-06-27 14:30:48 +0200

    change run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
 SEED=0
 
-python blind_knowledge_dist_training.py --dataset cifar10 --noise_type aggre --seed ${SEED} > cifar10_aggre_seed_${SEED}_training.log &
+mkdir results
+mkdir results/cifar10_aggre_seed_${SEED}
+mkdir results/cifar10_worst_seed_${SEED}
+mkdir results/cifar10_rand1_seed_${SEED}
 
-python blind_knowledge_dist_training.py --dataset cifar10 --noise_type worst --seed ${SEED} > cifar10_worst_seed_${SEED}_training.log &
+python blind_knowledge_dist_training.py --dataset cifar10 --noise_type aggre --seed ${SEED} > results/cifar10_aggre_seed_${SEED}/training.log &
 
-python blind_knowledge_dist_training.py --dataset cifar10 --noise_type rand1 --seed ${SEED} > cifar10_rand1_seed_${SEED}_training.log &
+python blind_knowledge_dist_training.py --dataset cifar10 --noise_type worst --seed ${SEED} > results/cifar10_worst_seed_${SEED}/training.log &
+
+python blind_knowledge_dist_training.py --dataset cifar10 --noise_type rand1 --seed ${SEED} > results/cifar10_rand1_seed_${SEED}/training.log
